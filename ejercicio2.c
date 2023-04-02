8828aa294dd8a02f362a910b1828a7b0afe16b7c
Andrea Valdecantos <andreavaldecantostorres@gmail.com>
2023-04-02 17:06:04 -0300

    promedio por año

--- ejercicio2.c
+++ ejercicio2.c
@@ -11,12 +11,25 @@ int main(){
             produccion[i][j] = rand() % 40001 + 10000;
         }
     }
+
     for (int i = 0; i < 5; i++){
         for (int j = 0; j < 12; j++){
             printf("%7d", produccion[i][j]);
         }
         printf("\r\n");
     }
+
+    //promedio de ganancia por año
+    for (int i = 0; i < 5; i++){
+        int suma = 0;
+        float promedio = 0;
+        for (int j = 0; j < 12; j++){
+            suma += produccion[i][j];
+        }
+        promedio = (float)suma/12;
+        printf("Promedio de ganancia anio %d: %.2f\r\n", i+1, promedio);
+    }
+    
 
     return 0;
 }
