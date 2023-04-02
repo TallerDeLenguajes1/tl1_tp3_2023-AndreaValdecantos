526f2f834975c6603a69d57baf474fd8cf093c02
Andrea Valdecantos <andreavaldecantostorres@gmail.com>
2023-04-02 17:26:03 -0300

    valor mínimo y máximo

--- ejercicio2.c
+++ ejercicio2.c
@@ -13,6 +13,7 @@ int main(){
     }
 
     for (int i = 0; i < 5; i++){
+        printf("Anio %d", i+1);
         for (int j = 0; j < 12; j++){
             printf("%7d", produccion[i][j]);
         }
@@ -29,6 +30,26 @@ int main(){
         promedio = (float)suma/12;
         printf("Promedio de ganancia anio %d: %.2f\r\n", i+1, promedio);
     }
+
+    //valor máximo y mínimo
+    int minimo = 50000, maximo = 10000, mesMinimo, mesMaximo, anioMinimo, anioMaximo;
+
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 12; j++){
+            if (produccion[i][j] <= minimo){
+                minimo = produccion[i][j];
+                mesMinimo = j + 1;
+                anioMinimo = i + 1;
+            }
+            if (produccion[i][j] >= maximo){
+                maximo = produccion[i][j];
+                mesMaximo = j + 1;
+                anioMaximo = i + 1;
+            }
+        }
+    }
+    printf("Valor minimo: %d\tMes: %d\tAnio: %d\r\n", minimo, mesMinimo, anioMinimo);
+    printf("Valor maximo: %d\tMes: %d\tAnio: %d\r\n", maximo, mesMaximo, anioMaximo);
     
 
     return 0;
