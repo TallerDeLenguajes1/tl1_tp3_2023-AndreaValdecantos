7d84728e57b7a801a8c446e4e89c8d90ebd58904
Andrea Valdecantos <andreavaldecantostorres@gmail.com>
2023-04-03 13:42:30 -0300

    recibe la cantidad, pero no permite escribir todos los nombres

---
+++ ejercicio3v2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char **vectorNombres;
+    char *buff;
+    int cantidadDeNombres;
+
+    printf("Ingrese la cantidad de nombres");
+    scanf("%d", &cantidadDeNombres);
+    
+    vectorNombres = (char**)malloc(cantidadDeNombres*sizeof(char*));
+    buff = (char*)malloc(100*sizeof(char));
+
+    puts("Ingrese los nombres");
+    for (int i = 0; i < cantidadDeNombres; i++){
+        gets(buff);
+        vectorNombres[i] = (char*)malloc((strlen(buff)+1)*sizeof(char));
+        strcpy(vectorNombres[i], buff);
+    }
+    
+    printf("Nombres:\r\n");
+    for (int i = 0; i < cantidadDeNombres; i++){
+        printf("%s\r\n", vectorNombres[i]);
+        free(vectorNombres[i]);
+    }
+
+    free(buff);
+    free(vectorNombres);
+
+    return 0;
+}
