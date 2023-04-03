d5a28a13498d768ec77155b41abd248d977032e9
Andrea Valdecantos <andreavaldecantostorres@gmail.com>
2023-04-03 13:47:11 -0300

    solucionado con getchar()

--- ejercicio3v2.c
+++ ejercicio3v2.c
@@ -7,13 +7,14 @@ int main(){
     char *buff;
     int cantidadDeNombres;
 
-    printf("Ingrese la cantidad de nombres");
+    printf("Ingrese la cantidad de nombres: ");
     scanf("%d", &cantidadDeNombres);
     
     vectorNombres = (char**)malloc(cantidadDeNombres*sizeof(char*));
     buff = (char*)malloc(100*sizeof(char));
 
     puts("Ingrese los nombres");
+    getchar();
     for (int i = 0; i < cantidadDeNombres; i++){
         gets(buff);
         vectorNombres[i] = (char*)malloc((strlen(buff)+1)*sizeof(char));
